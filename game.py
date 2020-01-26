26f9c61e2972c02c9fea9e85322e15d629565b1c
hgocel <57999166+hgocel@users.noreply.github.com>
2020-01-26 17:02:20 -0500

    much closer to finishing game as a whole. need to add a bit more to story and work out a few more glitches.

--- game.py
+++ game.py
@@ -1,45 +1,52 @@
 import sys
 #introduction message
-print("Welcome to your new favorite game, the 100 percent accurate Magnet Simulator. Get ready to start! ")
+print("Welcome to your new favorite game, the 100 percent accurate Magnet Simulator. We'll start in the Magnet building.")
 
 def auditorium():
   '''first choice in the game'''
-  aud = input("You wake up in the MHS auditorium. do you: a) go back to sleep? b) get up and get back to the grind?  ")
+  print("You wake up in the MHS auditorium. Do you: ")
+  aud = input("a) go back to sleep? b) get up and get back to the grind?  ")
   if aud.lower() == "a":
     print("you foolish fool. you fall back to sleep, and Bruce sees and puts a pillow behind your head, but he also locks you in and you can't make it to class.")
     sys.exit()
   elif aud.lower() == "b":
     class1()
+  return aud
 
 def class1():
   '''second choice after leaving auditorium'''
-  onetwo = input("Now that you're up, do you go to class? a) nope b) of course!  ")
+  print("Now that you're up, do you go to class?")
+  onetwo = input("a) nope b) of course!  ")
   if onetwo.lower() == "a":
     print("how did you even get into this school?")
     sys.exit()
   elif onetwo.lower() == "b":
     participate()
+  return onetwo
 
 
 def participate():
   '''choice while in class1'''
-  part = input("will you participate in class? a) no thanks b) sure")
+  print("Will you participate in class?")
+  part = input("a) no thanks b) sure  ")
   if part.lower == "a":
     print("you vibe for a while, but the teacher calls on you anyway. have fun with what comes next.")
   elif part.lower == "b":
-    print("good for you. have fun with what comes next.")
+    print("good for you, but have fun with what comes next.")
   ANXIETY()
+  return part
   
 def ANXIETY():
   '''anxiety fight'''
   print("You hear tense battle music from somebody's headphones. ANXIETY appears!")
-  zozo = input("What do you do? a) FIGHT! b) aaa run!")
+  zozo = input("What do you do? a) FIGHT! b) aaa run!  ")
   if zozo == "a":
       print("good job, facing your fears was the right answer!")
       threefour()
   elif zozo == "b":
       print("you run away, making a screeching sound as you do so. This does nothing to help you at all and you fail.")
       sys.exit()
+  return zozo
 
 def threefour():
   '''second class of the day, after anxiety fight'''
@@ -48,22 +55,26 @@ def threefour():
   if class2 == "a":
       print("you feel stressed, but this is better in the long run.")
       Luck == True
+      Lunch()
   elif class2 == "b":
       print("you feel more relaxed, but this could be detrimental in the long run.  ")
-      Luck == False  
+      Luck == False
+      Harsh()  
+  return Luck
 
 def Lunch():
   '''after 3/4. only a choice if Luck is True'''
-  if Luck == True:
+  if threefour == True:
       lun = input("you can a) eat your lunch or b) wander around. what do you do?  ")
       if lun == "a":
           print("your hunger is sated.")
           Spanish()
       elif lun =="b":
           Harsh()
-  elif Luck == False:
+  elif threefour == False:
       print("you already ate your lunch, so you wander around.")
       Harsh()
+  return lun
 
 def Harsh():
   '''Hard to define, but here, he offers you a choice.'''
@@ -74,35 +85,55 @@ def Harsh():
   elif epic == "b":
       print("Harsh respects your decision, but is secretly disappointed in you. You die of shame.")
       sys.exit()
+  return epic
 
 def Spanish():
   '''choice after going to lunch.'''
   esp = input("spanish class. do you speak a) english or b) spanish this period?  ")
-  if esp == "a":
+  if esp.lower == "a":
       print("being monolingual? that's kinda cringe. try again.")
       sys.exit()
-  elif esp == "b":
+  elif esp.lower == "b":
       print("muy bien, you make it through spanish class.")
       Sans()
-  else:
-      esp = input("please choose a or b.  ")
-
-def intimidate():
-  '''choice one of the final fight''' 
+  return esp
 
 def popquiz():
-  '''choice two of the final fight'''
+  '''Mr. Sanservino's pop quiz'''
+  print("Mr. Sans laughs and gives you a pop quiz.")
+  print("Quick! What college are you going to?")
+  pop = input("a) Duke? b) literally anywhere else")
+  if pop == "a":
+    print("You get a 0 and you know why.")
+    sys.exit()
+  elif pop == "b":
+    print("Even if it's not NC State, Mr. Sans respects your choice. He gives you some pretty dope college advice.")
+    print("You win! You have achieved the rank: Master of Vibes.")
+    sys.exit()
+  return pop
 
 def Sans():
   '''the final boss fight.'''
   print("You hear a familiar voice. Megalovania begins to play... Mr. Sans(ervino) appears!")
   print("Mr. Sans stands menacingly before you... What now?")
-  ddda = input("a) stand your ground. b) beg for a good grade c) up up down down left right left right b a b a start.  ")
-  if ddda == "a":
-      print("you t pose on the final boss. are you lucky enough for it to work?")
-      if Luck == True:
-          print("Mr. Sans recognizes what you are trying to do. it does not work, but he respects you enough to let you pass. You win!")
-          
+  #variable named after first four notes of megalovania, not just nonsense
+  DDdA = input("a) Stand your ground. b) Beg for a good grade c) Konami Code")
+  if DDdA.lower == "a":
+      print("You t pose on the final boss. are you lucky enough for it to work?")
+      if threefour == True:
+          print("Mr. Sans recognizes what you are trying to do. it does not work, but he respects you enough to let you pass.")
+          print("You win! You have achieved the rank: Responsible Badass")
+          sys.exit()
+      elif threefour == False:
+        popquiz()
+  elif DDdA.lower == "b":
+    print("You beg for a good grade.")
+    popquiz()
+  elif DDdA.lower == "c":
+    print("You input the Konami Code. Which one is correct?  ")
+    konami = input("a) up up down down left right left right b a b a start or b) left left up down right a b  ")
+    if konami.lower == "a":
+      print("Correct. You get 30 lives and $69, both of which are irrelevant in this game.")
+      print("You win? You have achieved the rank: Harsh-level Hacker")
   
 auditorium()
-#NTS return your functions before you wonder why they don't work
